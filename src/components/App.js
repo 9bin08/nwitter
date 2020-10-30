f76f8405ef9718ec363fe8411130159f62bbcbd0
9bin08 <9bin08@naver.com>
2020-10-31 01:02:26 +0900

    finsh git add .

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,43 @@
 import React, { useState, useEffect } from "react";
-import AppRouter from "../components/Router";
-import { authService } from "../myBase";
+import AppRouter from "components/Router";
+import { authService } from "myBase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
-        setUserObj(user);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
       } else {
-        setIsLoggedIn(false);
+        setUserObj(null);
       }
       setInit(true);
     });
   }, []);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+        />
       ) : (
-        "Init...."
+        "Initializing..."
       )}
-      <footer>&copy; {new Date().getFullYear()}Nwitter</footer>
     </>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { dbService } from "myBase";
-import Nweet from "../components/Nweet";
+import Nweet from "components/Nweet";
+import NweetFactory from "components/NweetFactory";
 
 const Home = ({ userObj }) => {
-  const [nweet, setNweet] = useState("");
-  const [nweets, setNweets] = useState([]);
   //   const getNweets = async () => {
   //     const dbnweets = await dbService.collection("nweets").get();
   //     dbnweets.forEach((document) => {
@@ -15,43 +14,23 @@ const Home = ({ userObj }) => {
   //       setNweets((prev) => [nweetObject, ...prev]);
   //     });
   //   };
+  const [nweets, setNweets] = useState([]);
   useEffect(() => {
-    dbService.collection("nweets").onSnapshot((snapshot) => {
-      const nweetArray = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setNweets(nweetArray);
-    });
+    dbService
+      .collection("nweets")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) => {
+        const nweetArray = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setNweets(nweetArray);
+      });
   }, []);
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    await dbService.collection("nweets").add({
-      text: nweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-    });
-    setNweet("");
-  };
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setNweet(value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={nweet}
-          onChange={onChange}
-          type="text"
-          placeholder="What's on your mind?"
-          maxLength={120}
-        />
-        <input type="submit" value="Nweet" />
-      </form>
-      <div>
+    <div className="container">
+      <NweetFactory userObj={userObj} />
+      <div style={{ marginTop: 30 }}>
         {nweets.map((nweet) => (
           <Nweet
             key={nweet.id}
